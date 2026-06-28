49a39653b704dc16605eca6f4e161c914291b558
YU <3084288207@qq.com>
2026-06-28 11:13:49 +0800

    前端：设备状态改造、点位查看详情、策略管理改造、设备策略分配

---
+++ dkd-vue/src/views/manage/vmStatus/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="设备编号" prop="innerCode">
+        <el-input v-model="queryParams.innerCode" placeholder="请输入设备编号" clearable @keyup.enter="handleQuery" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table v-loading="loading" :data="vmList" @selection-change="handleSelectionChange">
+      <el-table-column label="序号" type="index" width="55" align="center" />
+      <el-table-column label="设备编号" align="center" prop="innerCode" />
+      <el-table-column label="设备型号" align="center" prop="vmTypeId">
+        <template #default="scope">
+          <div v-for="item in vmTypeList" :key="item.id">
+            <span v-if="item.id==scope.row.vmTypeId">{{ item.name }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="详细地址" align="left" prop="addr" show-overflow-tooltip="true"/>
+      <el-table-column label="运营状态" align="center" prop="vmStatus">
+        <template #default="scope">
+          <dict-tag :options="vm_status" :value="scope.row.vmStatus"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="设备状态" align="center" prop="vmStatus">
+        <template #default="scope">
+         {{ scope.row.runningStatus!=null? JSON.parse(scope.row.runningStatus).status==true?'正常':'异常' :'异常'}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" @click="getVmInfo(scope.row)" v-hasPermi="['manage:vm:query']">查看详情</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="VmStatus">
+import { listVm, getVm } from "@/api/manage/vm";
+import { listVmType } from "@/api/manage/vmType";
+import { loadAllParams } from "@/api/page";
+
+const { proxy } = getCurrentInstance();
+const { vm_status } = proxy.useDict('vm_status');
+
+const vmList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const vmTypeList = ref([]);
+function getVmTypeList() {
+  listVmType(loadAllParams).then(response => {
+    vmTypeList.value = response.rows;
+  });
+}
+getVmTypeList();
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    innerCode: null,
+  },
+  rules: {}
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+function getList() {
+  loading.value = true;
+  listVm(queryParams.value).then(response => {
+    vmList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+function reset() {
+  form.value = {
+    id: null,
+    innerCode: null,
+    channelMaxCapacity: null,
+    nodeId: null,
+    addr: null,
+    lastSupplyTime: null,
+    businessType: null,
+    regionId: null,
+    partnerId: null,
+    vmTypeId: null,
+    vmStatus: null,
+    runningStatus: null,
+    longitudes: null,
+    latitude: null,
+    clientId: null,
+    policyId: null,
+    createTime: null,
+    updateTime: null
+  };
+  proxy.resetForm("vmRef");
+}
+
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+function getVmInfo(row) {
+  reset();
+  const _id = row.id || ids.value;
+  getVm(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "设备详情";
+  });
+}
+
+getList();
+</script>
